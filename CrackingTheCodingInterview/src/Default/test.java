423b9fb56c2677845bbf7c11608535c953d37ec5
ChrisMSG <sombra_neon@hotmail.com>
2020-09-27 17:30:56 -0700

    Test comment

---
+++ CrackingTheCodingInterview/src/Default/test.java
@@ -0,0 +1,11 @@
+package Default;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Hello2");
+	}
+
+}
